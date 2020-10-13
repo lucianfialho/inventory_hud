fca0644f3337200970e076477194d99fc85f4ce9
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-12 22:47:09 -0300

    -- criando split de objetos para facilitar o drop dos items

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,79 @@
 <template>
   <div id="app" v-if="showInventoryHud">
-    <draggable class="list-group" :list="inventory" group="people" handle=".info" dragoverBubble="true" forceFallback="false">
-        <template v-for="element in inventory">
-          <div
-            class="list-group-item"
-            v-bind:class="{ active: element.context }"
-            v-if="element.count"
-            :key="element.name"
-            @contextmenu.prevent="element.context = !element.context"
-          >
-            <div class="info">
-              <div class="bar">
-                <span class="weight"><b-tag rounded> {{element.weight}}kg</b-tag></span>
-                <span class="quantity"><b-tag rounded> {{element.count}}</b-tag></span>
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">{{user.firstName}} {{user.lastName}}</p>
+            <p class="subtitle is-6">045-933</p>
+          </div>
+        </div>
+
+        <div class="content">
+          <time :datetime="user.dateOfBirth">{{user.dateOfBirth}}</time>
+        </div>
+      </div>
+    </div>
+    <div class="inventory is-two-third">
+      <draggable class="list-group" :list="inventory" group="people" handle=".info" dragoverBubble="true" forceFallback="false">
+          <template v-for="(element, index) in inventory">
+            <div
+              class="list-group-item"
+              v-bind:class="{ active: element.context }"
+              v-if="element.count"
+              :key="index"
+              @contextmenu.prevent="useItem(element)"
+              @click.middle.exact="element.context = !element.context"
+              @click.middle.ctrl.exact="splitItem(element)"
+            >
+              <div class="info">
+                <div class="bar">
+                  <span class="weight"><b-tag rounded> {{element.weight}}kg</b-tag></span>
+                  <span class="quantity"><b-tag rounded> {{element.count}}</b-tag></span>
+                </div>
+
+                <b-icon
+                  pack="fas"
+                  :icon="element.icon ? element.icon : 'question'"
+                  size="is-medium">
+                </b-icon>
+                <span> {{element.label}} </span>
               </div>
 
-              <b-icon
-                pack="fas"
-                :icon="element.icon ? element.icon : 'question'"
-                size="is-medium">
-              </b-icon>
-              <span> {{element.label}} </span>
+              <div v-if="element.context" class="menu-context">
+                <b-field>
+                  <b-input placeholder="Throw"
+                      v-if="element.canRemove"
+                      size="is-small"
+                      v-model="element.dropQuantity"
+                      min="1"
+                      :max="element.count"
+                      type="number"
+                      icon-right="trash"
+                      icon-right-clickable
+                      @icon-right-click="dropItem(element)">
+                  </b-input>
+                </b-field>
+              </div>
             </div>
+          </template>
+      </draggable>
 
-            <div v-if="element.context" class="menu-context">
-              <b-button size="is-small"
-                icon-left="user-check"
-                @click.prevent="useItem(element)"
-                >
-                Use
-              </b-button>
-              <b-field>
-                <b-input placeholder="Throw"
-                    v-if="element.canRemove"
-                    size="is-small"
-                    v-model="element.dropQuantity"
-                    min="1"
-                    :max="element.count"
-                    type="number"
-                    icon-right="trash"
-                    icon-right-clickable
-                    @icon-right-click="dropItem(element)">
-                </b-input>
-              </b-field>
-            </div>
-          </div>
-        </template>
-    </draggable>
-
-    <draggable class="list-group drop-zone" :list="floor" group="people" dragoverBubble="true" forceFallback="false" @change="dropItem">
-      <div
-        class="list-group-item"
-        v-for="element in floor"
-        :key="element.name"
-      >
-        {{ element.label }}
-      </div>
-    </draggable>
+      <draggable class="list-group drop-zone" :list="floor" group="people" dragoverBubble="true" forceFallback="false" @change="dropItem">
+        <div
+          class="list-group-item"
+          v-for="element in floor"
+          :key="element.name"
+        >
+          {{ element.label }}
+        </div>
+      </draggable>
+    </div>
   </div>
 </template>
 
@@ -71,6 +88,11 @@ export default {
 
   data() {
     return {
+      user: {
+        firstName: 'Maria',
+        lastName: 'Joana',
+        dateOfBirth: '16/08/1989'
+      },
       showInventoryHud: false,
       inventory: [
         {"name": "bandage", "label": "Bandage", "weight": 2, "rare": 0, "canRemove": 1, "icon": "band-aid", "count": 4, "context": false, dropQuantity: 0},
@@ -96,6 +118,8 @@ export default {
         if(item.showInventoryHud === false) this.showInventoryHud = item.showInventoryHud
 
         if(item.items) this.inventory = item.items
+
+        if(item.user) this.user = item.user
       },
       false,
     );
@@ -109,11 +133,28 @@ export default {
     },
     dropItem (element) {
       const item = (element.hasOwnProperty('added')) ? element.added.element : element
-      const hasDropped = this.sendData('esx_inventory_hud:DropItem', item)
+      const hasDropped = this.sendData('esx_inventory_hud:DropItem', parseInt(item))
             hasDropped.then(response => {
               if (response.data) this.floor.splice(0, 1) 
             })
     },
+    splitItem (element) {
+
+      if (element.count < 2) return
+
+      let countObj1 = parseInt(element.count / 2)
+      let countObj2 = element.count - countObj1
+      
+      const obj1 = {...element, count: countObj1}
+      const obj2 = {...element, count: countObj2}
+      
+      this.insertInInventory([obj1, obj2])
+      
+    },
+    insertInInventory (arrayOfItems) {
+      let newinventory = this.inventory.filter(el => (el.name !== arrayOfItems[0].name))
+      this.inventory = [...newinventory, ...arrayOfItems]
+    } 
   },
 };
 </script>
@@ -128,11 +169,21 @@ html {
 }
 #app {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   background: rgba(0,0,0,.125);
   height: 100%;
   position: absolute;
   width: 100%;
+}
+.card {
+  min-width: 25% !important;
+  max-width: 30% !important;
+  max-height: 20%;
+  margin: 2%;
+}
+.inventory {
+  display: flex;
+  flex-direction: column;
 }
 .buttons {
   margin-top: 35px;
@@ -142,7 +193,7 @@ html {
   background: #c8ebfb;
 }
 .list-group {
-    width: 35%;
+    width: 100%;
     margin: 2%;
     min-height: 200px;
 
