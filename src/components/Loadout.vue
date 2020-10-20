665b2ea563bf9d710b22518f13b60fb547b4e97e
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-20 12:36:01 -0300

    -- add algumas validacoes no loadout

--- src/components/Loadout.vue
+++ src/components/Loadout.vue
@@ -32,6 +32,9 @@
 </template>
 
 <script>
+
+import Nui from '@/utils/Nui'
+
 export default {
   name: 'loadout',
   props: {
@@ -57,7 +60,45 @@ export default {
     },
     checkWeaponSelected () {
         return this.loadout.filter((weapon) => (weapon.selected)).length > 0
+    },
+    getWeaponSelected () {
+        return this.loadout.filter((weapon) => (weapon.selected))
     }
+  },
+  mounted () {
+        this._keyListener = function(e) {
+            const keys = ["1", "2", "3", "4", "5", "6"]
+            if (keys.includes(e.key) && (e.ctrlKey)) {
+                e.preventDefault();
+                
+                let weapon = this.getWeaponSelected()[0]
+                
+                if(weapon == null || weapon == undefined) {
+                    this.$buefy.snackbar.open({
+                        message: this.$i18n.t('notifications.needWeaponSelect'),
+                        type: 'is-warning',
+                        position: 'is-top',
+                        indefinite: true,
+                    })
+                    return 
+                }
+                
+                if(weapon.type === "item_weapon") {
+                    weapon.bind = e.key
+                    Nui.sendData('esx_inventory_hud:SetWeaponBinding', weapon)
+                }else {
+                     this.$buefy.snackbar.open({
+                        message: this.$i18n.t('notifications.onlyWeaponCanBeBinding'),
+                        type: 'is-warning',
+                        position: 'is-top',
+                        indefinite: true,
+                    })
+                }
+            }
+        };
+
+        document.addEventListener('keydown', this._keyListener.bind(this));
+
   }
 };
 </script>
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -7,14 +7,18 @@ const messages = {
         loadout: 'Loadout',
         items: 'Items',
         notifications:{
-            moreThanOneWeaponSelect: 'You can select only 1 weapon!'
+            moreThanOneWeaponSelect: 'You can select only 1 weapon!',
+            needWeaponSelect: 'You need a weapon selected',
+            onlyWeaponCanBeBinding: 'Only weapon can be binding'
         }
     },
     'pt-br': {
         loadout: 'Armamentos',
         items: 'Equipamentos',
         notifications:{
-            moreThanOneWeaponSelect: 'Você só pode selecionar uma arma!'
+            moreThanOneWeaponSelect: 'Você só pode selecionar uma arma!',
+            needWeaponSelect: 'Você precisa ter uma arma selecionada',
+            onlyWeaponCanBeBinding: 'Somente armas podem ser bindadas'
         }
     }
 };
