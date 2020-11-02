24cde8dd5fea5dfe80c9e880b8978592eae6ea26
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-11-02 16:34:47 -0300

    -- separando metodos do player

--- src/utils/Player.js
+++ src/utils/Player.js
@@ -5,8 +5,12 @@ export default {
         
         // Desacoplando a função que pega os players proximos, quero usar ela no de arma tbm
         const closestsPlayers = await Nui.sendData('esx_inventory_hud:GetClosestsPlayers', element)
-
-        return closestsPlayers.data
+        
+        return closestsPlayers.data.playersClosest
     },
 
+    async giveItemToPlayer (playerToGive, element) {
+        const givemItem = await Nui.sendData('esx_inventory_hud:GiveItemToAPlayer', {playerToGive: playerToGive, element: element})
+        return givemItem.data
+    },
 }
